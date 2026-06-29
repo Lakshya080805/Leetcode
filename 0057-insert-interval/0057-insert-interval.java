1fb5862935de1a9e20a57bd3dc1aea903b93cb93
Lakshya Singhal <143376209+Lakshya080805@users.noreply.github.com>
2026-06-29 15:47:11 +0530

    Time: 1 ms (97.89%), Space: 46.9 MB (88.39%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,41 +1,29 @@
-
-class Meeting {
-    int start, end;
-    
-    Meeting(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-}
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        Meeting[] meetings = new Meeting[intervals.length + 1];
-
-        for (int i = 0; i < intervals.length; i++) {
-            meetings[i] = new Meeting(intervals[i][0], intervals[i][1]);
-        }
-
-        meetings[intervals.length] = new Meeting(newInterval[0], newInterval[1]);
-
-        Arrays.sort(meetings, (a, b) -> Integer.compare(a.start, b.start));
-
         List<int[]> ans = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
 
-        int start = meetings[0].start;
-        int end = meetings[0].end;
-
-        for (int i = 1; i < meetings.length; i++) {
-
-            if (meetings[i].start <= end) {
-                end = Math.max(end, meetings[i].end);
-            } else {
-                ans.add(new int[]{start, end});
-                start = meetings[i].start;
-                end = meetings[i].end;
-            }
+        // 1. Add all intervals before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.add(intervals[i]);
+            i++;
         }
 
-        ans.add(new int[]{start, end});
+        // 2. Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        ans.add(newInterval);
+
+        // 3. Add remaining intervals
+        while (i < n) {
+            ans.add(intervals[i]);
+            i++;
+        }
 
         return ans.toArray(new int[ans.size()][]);
     }
