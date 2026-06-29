c62c59fb2671d9c31800e86ba1bc88ec2d776502
Lakshya Singhal <143376209+Lakshya080805@users.noreply.github.com>
2026-06-29 15:34:27 +0530

    Time: 8 ms (91.24%), Space: 49.3 MB (26.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-         if (intervals.length <= 1) {
+        if (intervals.length <= 1) {
             return intervals;
         }
  
