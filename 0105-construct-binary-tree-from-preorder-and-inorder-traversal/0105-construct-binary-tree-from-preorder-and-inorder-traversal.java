d2b34a29e7febe9a5404eb8bb1eebe5f66efb026
Lakshya Singhal <143376209+Lakshya080805@users.noreply.github.com>
2026-07-02 20:59:53 +0530

    Time: 1 ms (97.57%), Space: 44.7 MB (99.88%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer,Integer> mpp=new HashMap<>();
+
+        for(int i=0;i<inorder.length;i++){
+            mpp.put(inorder[i],i);
+        }
+
+        TreeNode root=buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,mpp);
+
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder,int preStart,int preEnd,int[] inorder,int inStart,int inEnd,Map<Integer,Integer> mpp){
+        if(preStart>preEnd || inStart>inEnd) return null;
+
+        TreeNode root=new TreeNode(preorder[preStart]);
+
+        int idxRoot=mpp.get(root.val);
+
+        int numsLeft=idxRoot-inStart;
+
+        root.left=buildTree(preorder,preStart+1,preStart+numsLeft,inorder,inStart,idxRoot-1,mpp);
+
+        root.right=buildTree(preorder,preStart+numsLeft+1,preEnd,inorder,idxRoot+1,inEnd,mpp);
+
+        return root;
+    }
+}
