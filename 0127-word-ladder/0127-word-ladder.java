819878fafea3379eb0861560f89489d4d37d7135
Lakshya Singhal <143376209+Lakshya080805@users.noreply.github.com>
2026-07-06 16:18:10 +0530

    Time: 91 ms (34.46%), Space: 46.3 MB (98.54%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,39 @@
+class Pair{
+    String first;
+    int second;
+    Pair(String _first,int _second){
+        first=_first;
+        second=_second;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(beginWord,1));
+        Set<String> st=new HashSet<>();
+        for(int i=0;i<wordList.size();i++){
+            st.add(wordList.get(i));
+        }
+        st.remove(beginWord);
+        while(!q.isEmpty()){
+            String word=q.peek().first;
+            int steps=q.peek().second;
+            q.remove();
+            if(word.equals(endWord))return steps;
+            for(int i=0;i<word.length();i++){
+                for(char ch='a';ch<='z';ch++){
+                    char replacedCharArray[]=word.toCharArray();
+                    replacedCharArray[i]=ch;
+                    String replacedWord=new String(replacedCharArray);
+                    if(st.contains(replacedWord)==true){
+                        st.remove(replacedWord);
+                        q.add(new Pair(replacedWord,steps+1));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
